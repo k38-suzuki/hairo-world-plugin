4bf791641809bff6f53dc9f8f39f38c83bfb3605
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2021-11-21 22:23:50 -0800

    fix history manager

--- src/BookmarkPlugin/HistoryManager.cpp
+++ src/BookmarkPlugin/HistoryManager.cpp
@@ -7,7 +7,6 @@
 #include <cnoid/AppConfig>
 #include <cnoid/MenuManager>
 #include <cnoid/ProjectManager>
-#include <vector>
 #include "gettext.h"
 
 using namespace cnoid;
@@ -19,16 +18,10 @@ class HistoryManagerImpl
 {
 public:
     HistoryManagerImpl(HistoryManager* self, ExtensionManager* ext);
-    virtual ~HistoryManagerImpl();
     HistoryManager* self;
 
-    ExtensionManager* ext;
-    int maxHistory;
-    vector<string> histories;
     ProjectManager* pm;
     MappingPtr config;
-
-    void onProjectLoaded(const string& filename);
 };
 
 }
@@ -42,21 +35,46 @@ HistoryManager::HistoryManager(ExtensionManager* ext)
 
 HistoryManagerImpl::HistoryManagerImpl(HistoryManager* self, ExtensionManager* ext)
     : self(self),
-      pm(ProjectManager::instance()),
-      ext(ext)
+      pm(ProjectManager::instance())
 {
-    maxHistory = 10;
-    histories.clear();
+    MenuManager& mm = ext->menuManager().setPath("/Tools").setPath(_("History"));
+    Menu* menu = mm.currentMenu();
+    menu->sigTriggered().connect([&](QAction* action){
+        string filename = action->text().toStdString();
+        pm->loadProject(filename);
+    });
 
     config = AppConfig::archive()->openMapping("history");
     int numHistories = config->get("num_history", 0);
     for(int i = 0; i < numHistories; ++i) {
         string key = "history_" + to_string(i);
         string history = config->get(key, "");
-        onProjectLoaded(history);
+        mm.addItem(history);
     }
 
-    pm->sigProjectLoaded().connect([&](int level){ onProjectLoaded(pm->currentProjectFile()); });
+    pm->sigProjectLoaded().connect([&](int level){
+        string filename = pm->currentProjectFile();
+        if(!filename.empty()) {
+            mm.addItem(filename);
+
+            int maxHistory = 10;
+            Menu* menu = mm.currentMenu();
+            int numHistories = menu->actions().size();
+            if(numHistories > maxHistory) {
+                Action* action = dynamic_cast<Action*>(menu->actions()[0]);
+                menu->removeAction(action);
+            }
+
+            numHistories = menu->actions().size();
+            config->write("num_history", numHistories);
+            for(int i = 0; i < numHistories; ++i) {
+                Action* action = dynamic_cast<Action*>(menu->actions()[i]);
+                string key = "history_" + to_string(i);
+                string filename = action->text().toStdString();
+                config->write(key, filename);
+            }
+        }
+    });
 }
 
 
@@ -66,47 +84,7 @@ HistoryManager::~HistoryManager()
 }
 
 
-HistoryManagerImpl::~HistoryManagerImpl()
-{
-    int numHistories = histories.size();
-    config->write("num_history", numHistories);
-    for(int i = 0; i < numHistories; ++i) {
-        string key = "history_" + to_string(i);
-        string history = histories[i];
-        config->write(key, history);
-    }
-}
-
-
 void HistoryManager::initialize(ExtensionManager* ext)
 {
-    HistoryManager* manager = ext->manage(new HistoryManager(ext));
-
-    MenuManager& mm = ext->menuManager().setPath("/Tools").setPath(_("History"));
-}
-
-
-void HistoryManagerImpl::onProjectLoaded(const string& filename)
-{
-    MenuManager& mm = ext->menuManager().setPath("/Tools").setPath(_("History"));
-
-    if(!filename.empty()) {
-        Action* historyItem = mm.addItem(filename);
-        historyItem->sigTriggered().connect([=](){ pm->loadProject(filename); });
-
-        histories.push_back(filename);
-        if(histories.size() > maxHistory) {
-            histories.erase(histories.begin());
-        }
-
-        QWidget* current = mm.current();
-        QMenu* menu = dynamic_cast<QMenu*>(current);
-        if(menu) {
-            int numHistories = menu->actions().size();
-            if(numHistories > maxHistory) {
-                Action* action = dynamic_cast<Action*>(menu->actions()[0]);
-                menu->removeAction(action);
-            }
-        }
-    }
+    ext->manage(new HistoryManager(ext));
 }
