36bb409e4221f11d71a11eea82eb448852390504
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2021-11-21 23:16:09 -0800

    fix effector config

--- src/VisualEffectPlugin/VisualEffectorItem.cpp
+++ src/VisualEffectPlugin/VisualEffectorItem.cpp
@@ -6,6 +6,13 @@
 #include "VisualEffectorItem.h"
 #include <cnoid/Archive>
 #include <cnoid/Body>
+#include <cnoid/BodyItem>
+#include <cnoid/Button>
+#include <cnoid/CheckBox>
+#include <cnoid/ComboBox>
+#include <cnoid/ConnectionSet>
+#include <cnoid/Dialog>
+#include <cnoid/ImageableItem>
 #include <cnoid/ItemManager>
 #include <cnoid/ItemTreeView>
 #include <cnoid/MenuManager>
@@ -13,9 +20,82 @@
 #include <cnoid/RootItem>
 #include "gettext.h"
 #include "VEAreaItem.h"
+#include <cnoid/RangeCamera>
+#include <cnoid/Separator>
+#include <cnoid/SpinBox>
+#include <QDialogButtonBox>
+#include <QGridLayout>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <math.h>
+#include "ImageGenerator.h"
 
 using namespace cnoid;
 using namespace std;
+
+namespace {
+
+class EffectConfigDialog : public Dialog
+{
+public:
+    EffectConfigDialog();
+
+    DoubleSpinBox* hueSpin;
+    DoubleSpinBox* saturationSpin;
+    DoubleSpinBox* valueSpin;
+    DoubleSpinBox* redSpin;
+    DoubleSpinBox* greenSpin;
+    DoubleSpinBox* blueSpin;
+    DoubleSpinBox* coefBSpin;
+    DoubleSpinBox* coefDSpin;
+    DoubleSpinBox* stdDevSpin;
+    DoubleSpinBox* saltSpin;
+    DoubleSpinBox* pepperSpin;
+    CheckBox* flipCheck;
+    ComboBox* filterCombo;
+
+    void onResetButtonClicked();
+    bool storeState(Archive& archive);
+    bool restoreState(const Archive& archive);
+};
+
+
+class VisualEffectorItemBase
+{
+public:
+    Item* visualizerItem;
+    BodyItem* bodyItem;
+    ScopedConnection sigCheckToggledConnection;
+    VisualEffectorItemBase(Item* visualizerItem);
+    void setBodyItem(BodyItem* bodyItem);
+    void updateVisualization();
+
+    virtual void enableVisualization(bool on) = 0;
+    virtual void doUpdateVisualization() = 0;
+};
+
+
+class VEImageVisualizerItem : public Item, public ImageableItem, public VisualEffectorItemBase
+{
+public:
+    VEImageVisualizerItem();
+    virtual const Image* getImage() override;
+    virtual SignalProxy<void()> sigImageUpdated() override;
+    void setBodyItem(BodyItem* bodyItem, Camera* camera);
+    virtual void enableVisualization(bool on) override;
+    virtual void doUpdateVisualization() override;
+
+    CameraPtr camera;
+    EffectConfigDialog* config;
+    ImageGenerator generator;
+    ScopedConnectionSet connections;
+    std::shared_ptr<const Image> image;
+    Signal<void()> sigImageUpdated_;
+};
+
+}
+
 
 namespace cnoid {
 
@@ -46,7 +126,7 @@ void VisualEffectorItem::initializeClass(ExtensionManager* ext)
         [](VEImageVisualizerItem* item, MenuManager& menuManager, ItemFunctionDispatcher menuFunction) {
             menuManager.setPath("/");
             menuManager.addItem(_("Visual Effect"))->sigTriggered().connect(
-                [item](){ item->effector->show(); });
+                        [item](){ item->config->show(); });
             menuManager.setPath("/");
             menuManager.addSeparator();
             menuFunction.dispatchAs<Item>(item);
@@ -158,8 +238,8 @@ bool VisualEffectorItem::store(Archive& archive)
         }
         item->store(*subArchive);
         VEImageVisualizerItem* vitem = dynamic_cast<VEImageVisualizerItem*>(item);
-        VisualEffector* effector = vitem->effector;
-        effector->store(*subArchive);
+        EffectConfigDialog* config = vitem->config;
+        config->storeState(*subArchive);
         subItems->append(subArchive);
     }
 
@@ -195,8 +275,8 @@ bool VisualEffectorItem::restore(const Archive& archive)
                 Item* tmpItem = item;
                 if(tmpItem) {
                     VEImageVisualizerItem* vitem = dynamic_cast<VEImageVisualizerItem*>(tmpItem);
-                    VisualEffector* effector = vitem->effector;
-                    effector->restore(*subArchive);
+                    EffectConfigDialog* config = vitem->config;
+                    config->restoreState(*subArchive);
                 }
                 impl->restoredSubItems.push_back(item);
             }
@@ -234,7 +314,7 @@ void VisualEffectorItemBase::updateVisualization()
 VEImageVisualizerItem::VEImageVisualizerItem()
     : VisualEffectorItemBase(this)
 {
-    effector = new VisualEffector();
+    config = new EffectConfigDialog();
 }
 
 
@@ -282,19 +362,19 @@ void VEImageVisualizerItem::enableVisualization(bool on)
 void VEImageVisualizerItem::doUpdateVisualization()
 {
     if(camera){
-        double hue = effector->hue();
-        double saturation = effector->saturation();
-        double value = effector->value();
-        double red = effector->red();
-        double green = effector->green();
-        double blue = effector->blue();
-        bool flipped = effector->flip();
-        double coefB = effector->coefB();
-        double coefD = effector->coefD();
-        double stdDev = effector->stdDev();
-        double salt = effector->salt();
-        double pepper = effector->pepper();
-        int filter = effector->filter();
+        double hue = config->hueSpin->value();
+        double saturation = config->saturationSpin->value();
+        double value = config->valueSpin->value();
+        double red = config->redSpin->value();
+        double green = config->greenSpin->value();
+        double blue = config->blueSpin->value();
+        bool flipped = config->flipCheck->isChecked();
+        double coefB = config->coefBSpin->value();
+        double coefD = config->coefDSpin->value();
+        double stdDev = config->stdDevSpin->value();
+        double salt = config->saltSpin->value();
+        double pepper = config->pepperSpin->value();
+        int filter = config->filterCombo->currentIndex();
 
         RootItem* rootItem = RootItem::instance();
         if(rootItem) {
@@ -355,4 +435,208 @@ void VEImageVisualizerItem::doUpdateVisualization()
         image.reset();
     }
     sigImageUpdated_();
+}
+
+
+EffectConfigDialog::EffectConfigDialog()
+{
+    setWindowTitle(_("Effect Config"));
+
+    const double ranges[11][2] = {
+        {  0.0, 1.0 }, { -1.0,  1.0 }, { -1.0, 1.0 },
+        { -1.0, 1.0 }, { -1.0,  1.0 }, { -1.0, 1.0 },
+        { -1.0, 0.0 }, {  1.0, 32.0 },
+        {  0.0, 1.0 }, {  0.0,  1.0 }, {  0.0, 1.0 }
+    };
+
+    hueSpin = new DoubleSpinBox();
+    saturationSpin = new DoubleSpinBox();
+    valueSpin = new DoubleSpinBox();
+    redSpin = new DoubleSpinBox();
+    greenSpin = new DoubleSpinBox();
+    blueSpin = new DoubleSpinBox();
+    coefBSpin = new DoubleSpinBox();
+    coefDSpin = new DoubleSpinBox();
+    stdDevSpin = new DoubleSpinBox();
+    saltSpin = new DoubleSpinBox();
+    pepperSpin = new DoubleSpinBox();
+    flipCheck = new CheckBox();
+    filterCombo = new ComboBox();
+
+    vector<DoubleSpinBox*> dspins;
+    dspins.push_back(hueSpin);
+    dspins.push_back(saturationSpin);
+    dspins.push_back(valueSpin);
+    dspins.push_back(redSpin);
+    dspins.push_back(greenSpin);
+    dspins.push_back(blueSpin);
+    dspins.push_back(coefBSpin);
+    dspins.push_back(coefDSpin);
+    dspins.push_back(stdDevSpin);
+    dspins.push_back(saltSpin);
+    dspins.push_back(pepperSpin);
+
+    for(size_t i = 0; i < dspins.size(); ++i) {
+        DoubleSpinBox* dspin = dspins[i];
+        dspin->setRange(ranges[i][0], ranges[i][1]);
+        dspin->setSingleStep(0.1);
+    }
+    coefDSpin->setValue(1.0);
+    flipCheck->setText(_("Flip"));
+    flipCheck->setChecked(false);
+    QStringList filters = { _("No filter"), _("Gaussian 3x3"), _("Gaussian 5x5"), _("Sobel"), _("Prewitt") };
+    filterCombo->addItems(filters);
+    filterCombo->setCurrentIndex(0);
+
+    QGridLayout* gbox = new QGridLayout();
+    int index = 0;
+    gbox->addWidget(new QLabel(_("Hue")), index, 0);
+    gbox->addWidget(hueSpin, index, 1);
+    gbox->addWidget(new QLabel(_("Saturation")), index, 2);
+    gbox->addWidget(saturationSpin, index, 3);
+    gbox->addWidget(new QLabel(_("Value")), index, 4);
+    gbox->addWidget(valueSpin, index++, 5);
+    gbox->addWidget(new QLabel(_("Red")), index, 0);
+    gbox->addWidget(redSpin, index, 1);
+    gbox->addWidget(new QLabel(_("Green")), index, 2);
+    gbox->addWidget(greenSpin, index, 3);
+    gbox->addWidget(new QLabel(_("Blue")), index, 4);
+    gbox->addWidget(blueSpin, index++, 5);
+    gbox->addWidget(new QLabel(_("CoefB")), index, 0);
+    gbox->addWidget(coefBSpin, index, 1);
+    gbox->addWidget(new QLabel(_("CoefD")), index, 2);
+    gbox->addWidget(coefDSpin, index++, 3);
+    gbox->addWidget(new QLabel(_("Std_dev")), index, 0);
+    gbox->addWidget(stdDevSpin, index, 1);
+    gbox->addWidget(new QLabel(_("Salt")), index, 2);
+    gbox->addWidget(saltSpin, index, 3);
+    gbox->addWidget(new QLabel(_("Pepper")), index, 4);
+    gbox->addWidget(pepperSpin, index++, 5);
+    gbox->addWidget(flipCheck, index, 0);
+    gbox->addWidget(new QLabel(_("Filter")), index, 2);
+    gbox->addWidget(filterCombo, index++, 3);
+
+    PushButton* resetButton = new PushButton(_("&Reset"));
+    QPushButton* okButton = new QPushButton(_("&Ok"));
+    okButton->setDefault(true);
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(this);
+    buttonBox->addButton(resetButton, QDialogButtonBox::ResetRole);
+    buttonBox->addButton(okButton, QDialogButtonBox::AcceptRole);
+    connect(buttonBox,SIGNAL(accepted()), this, SLOT(accept()));
+
+    QVBoxLayout* vbox = new QVBoxLayout();
+    vbox->addLayout(gbox);
+    vbox->addWidget(new HSeparator());
+    vbox->addWidget(buttonBox);
+    setLayout(vbox);
+
+    resetButton->sigClicked().connect([&](){ onResetButtonClicked(); });
+}
+
+
+void EffectConfigDialog::onResetButtonClicked()
+{
+    hueSpin->setValue(0.0);
+    saturationSpin->setValue(0.0);
+    valueSpin->setValue(0.0);
+    redSpin->setValue(0.0);
+    greenSpin->setValue(0.0);
+    blueSpin->setValue(0.0);
+    coefBSpin->setValue(0.0);
+    coefDSpin->setValue(1.0);
+    stdDevSpin->setValue(0.0);
+    saltSpin->setValue(0.0);
+    pepperSpin->setValue(0.0);
+    flipCheck->setChecked(false);
+    filterCombo->setCurrentIndex(0);
+}
+
+
+bool EffectConfigDialog::storeState(Archive& archive)
+{
+    archive.write("hue", hueSpin->value());
+    archive.write("saturation", saturationSpin->value());
+    archive.write("value", valueSpin->value());
+    archive.write("red", redSpin->value());
+    archive.write("green", greenSpin->value());
+    archive.write("blue", blueSpin->value());
+    archive.write("coef_b", coefBSpin->value());
+    archive.write("coef_d", coefDSpin->value());
+    archive.write("std_dev", stdDevSpin->value());
+    archive.write("salt", saltSpin->value());
+    archive.write("pepper", pepperSpin->value());
+    archive.write("flip", flipCheck->isChecked());
+    archive.write("filter", filterCombo->currentIndex());
+    return true;
+}
+
+
+bool EffectConfigDialog::restoreState(const Archive& archive)
+{
+    double value;
+    archive.read("hue", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    hueSpin->setValue(value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    archive.read("saturation", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    saturationSpin->setValue(value);
+    archive.read("value", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    valueSpin->setValue(value);
+    archive.read("red", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    redSpin->setValue(value);
+    archive.read("green", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    greenSpin->setValue(value);
+    archive.read("blue", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    blueSpin->setValue(value);
+    archive.read("coef_b", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    coefBSpin->setValue(value);
+    archive.read("coef_d", value);
+    if(fabs(value) < 1.0) {
+        value = 1.0;
+    }
+    coefDSpin->setValue(value);
+    archive.read("std_dev", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    stdDevSpin->setValue(value);
+    archive.read("salt", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    saltSpin->setValue(value);
+    archive.read("pepper", value);
+    if(fabs(value) < 0.01) {
+        value = 0.0;
+    }
+    pepperSpin->setValue(value);
+    bool flip;
+    archive.read("flip", flip);
+    flipCheck->setChecked(flip);
+    int filter = 0;
+    archive.read("filter", filter);
+    filterCombo->setCurrentIndex(filter);
+    return true;
 }
--- src/VisualEffectPlugin/VisualEffectorItem.h
+++ src/VisualEffectPlugin/VisualEffectorItem.h
@@ -6,54 +6,12 @@
 #ifndef CNOID_VISUALEFFECTPLUGIN_VISUALEFFECTORITEM_H
 #define CNOID_VISUALEFFECTPLUGIN_VISUALEFFECTORITEM_H
 
-#include <cnoid/BodyItem>
-#include <cnoid/ConnectionSet>
-#include <cnoid/ImageableItem>
 #include <cnoid/Item>
-#include <cnoid/RangeCamera>
-#include "ImageGenerator.h"
-#include "VisualEffector.h"
 #include "exportdecl.h"
 
 namespace cnoid {
 
 class VisualEffectorItemImpl;
-
-class VisualEffectorItemBase
-{
-public:
-    Item* visualizerItem;
-    BodyItem* bodyItem;
-    ScopedConnection sigCheckToggledConnection;
-    VisualEffectorItemBase(Item* visualizerItem);
-    void setBodyItem(BodyItem* bodyItem);
-    void updateVisualization();
-
-    virtual void enableVisualization(bool on) = 0;
-    virtual void doUpdateVisualization() = 0;
-};
-
-
-class CNOID_EXPORT VEImageVisualizerItem : public Item, public ImageableItem, public VisualEffectorItemBase
-{
-public:
-    VEImageVisualizerItem();
-    virtual const Image* getImage() override;
-    virtual SignalProxy<void()> sigImageUpdated() override;
-    void setBodyItem(BodyItem* bodyItem, Camera* camera);
-    virtual void enableVisualization(bool on) override;
-    virtual void doUpdateVisualization() override;
-
-    CameraPtr camera;
-    VisualEffector* effector;
-    ImageGenerator generator;
-    ScopedConnectionSet connections;
-    std::shared_ptr<const Image> image;
-    Signal<void()> sigImageUpdated_;
-};
-
-typedef ref_ptr<VEImageVisualizerItem> VEImageVisualizerItemPtr;
-
 
 class CNOID_EXPORT VisualEffectorItem : public Item
 {
