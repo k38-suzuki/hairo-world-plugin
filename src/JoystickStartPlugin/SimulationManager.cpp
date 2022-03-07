170ea0f7be45f0b830cdd773e4b578be775c818d
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2022-03-06 21:36:45 -0800

    fix simulation-manager

--- src/JoystickStartPlugin/SimulationManager.cpp
+++ src/JoystickStartPlugin/SimulationManager.cpp
@@ -6,29 +6,16 @@
 #include "SimulationManager.h"
 #include <cnoid/Action>
 #include <cnoid/AppConfig>
-#include <cnoid/ExecutablePath>
-#include <cnoid/FileDialog>
-#include <cnoid/FileUtil>
 #include <cnoid/Joystick>
 #include <cnoid/JoystickCapture>
-#include <cnoid/MainWindow>
 #include <cnoid/MenuManager>
-#include <cnoid/MessageView>
 #include <cnoid/ProjectManager>
-#include <cnoid/RootItem>
+#include <cnoid/SimulationBar>
 #include <cnoid/SimulatorItem>
-#include <cnoid/TimeBar>
-#include <cnoid/WorldItem>
-#include <cnoid/stdx/filesystem>
-#include <QMessageBox>
-#include <fmt/format.h>
-#include <functional>
 #include "gettext.h"
 
-using namespace std;
 using namespace cnoid;
-namespace filesystem = stdx::filesystem;
-using fmt::format;
+using namespace std;
 
 namespace cnoid {
 
@@ -39,23 +26,14 @@ public:
   virtual ~SimulationManagerImpl();
   SimulationManager* self;
 
-  MessageView* mv;
   bool startState;
   bool pauseState;
-  string currentProjectName;
-  string currentProjectFile;
   JoystickCapture joystick;
   Action* useStartButton;
   Action* useLogoButton;
+  SimulatorItem* simulatoritem;
+  SimulationBar* sb;
 
-  void startSimulation(bool doReset = true);
-  void startSimulation(SimulatorItem* simulatorItem, bool doReset);
-  void stopSimulation();
-  void stopSimulation(SimulatorItem* simulatorItem);
-  void pauseSimulation();
-  void pauseSimulation(SimulatorItem* simulatorItem);
-  void forEachSimulator(std::function<void(SimulatorItem* simulatorItem)> callback, bool doSelect = false);
-  void openDialogToLoadProject();
   void onButton(const int& id, const bool& isPressed);
   void store(Mapping& archive);
   void restore(const Mapping& archive);
@@ -72,16 +50,20 @@ SimulationManager::SimulationManager(ExtensionManager* ext)
 
 SimulationManagerImpl::SimulationManagerImpl(SimulationManager* self, ExtensionManager* ext)
     : self(self),
-      mv(MessageView::instance())
+      sb(SimulationBar::instance())
 {
     MenuManager& mm = ext->menuManager().setPath("/Options").setPath(N_("Joystick"));
     useStartButton = mm.addCheckItem(_("Start simulation (StartButton)"));
-    useLogoButton = mm.addCheckItem(_("Open a project (LogoButton)"));
+//    useLogoButton = mm.addCheckItem(_("Open a project (LogoButton)"));
+    useLogoButton = new Action;
 
     startState = false;
     pauseState = false;
     joystick.setDevice("/dev/input/js0");
     joystick.sigButton().connect([&](int id, bool isPressed){ onButton(id, isPressed); });
+
+    sb->sigSimulationAboutToStart().connect(
+                [&](SimulatorItem* simulatorItem){ this->simulatoritem = simulatorItem; });
 
     Mapping& config = *AppConfig::archive()->openMapping("simulation_manager");
     if(config.isValid()) {
@@ -108,203 +90,40 @@ void SimulationManager::initialize(ExtensionManager* ext)
 }
 
 
-void SimulationManagerImpl::startSimulation(bool doReset)
-{
-    forEachSimulator([&, doReset](SimulatorItem* simulatorItem){ startSimulation(simulatorItem, doReset); }, true);
-    startState = true;
-}
-
-
-void SimulationManagerImpl::startSimulation(SimulatorItem* simulatorItem, bool doReset)
-{
-    if(simulatorItem->isRunning()) {
-        if(!doReset) {
-            simulatorItem->restartSimulation();
-        }
-        TimeBar::instance()->startPlayback();
-    } else {
-        simulatorItem->startSimulation(doReset);
-    }
-}
-
-
-void SimulationManagerImpl::stopSimulation()
-{
-    forEachSimulator([&](SimulatorItem* simulatorItem){ stopSimulation(simulatorItem); });
-    TimeBar* timeBar = TimeBar::instance();
-    if(timeBar->isDoingPlayback()) {
-        timeBar->stopPlayback();
-    }
-    startState = false;
-    pauseState = false;
-}
-
-
-void SimulationManagerImpl::stopSimulation(SimulatorItem* simulatorItem)
-{
-    simulatorItem->stopSimulation();
-}
-
-
-void SimulationManagerImpl::pauseSimulation()
-{
-    forEachSimulator([&](SimulatorItem* simulatorItem){ pauseSimulation(simulatorItem); });
-    pauseState = !pauseState;
-}
-
-
-void SimulationManagerImpl::pauseSimulation(SimulatorItem* simulatorItem)
-{
-    TimeBar* timeBar = TimeBar::instance();
-    if(!pauseState) {
-        if(simulatorItem->isRunning()) {
-            simulatorItem->pauseSimulation();
-        }
-        if(timeBar->isDoingPlayback()) {
-            timeBar->stopPlayback();
-        }
-    } else {
-        if(simulatorItem->isRunning()) {
-            simulatorItem->restartSimulation();
-        }
-        timeBar->startPlayback();
-    }
-}
-
-
-void SimulationManagerImpl::forEachSimulator(std::function<void(SimulatorItem* simulatorItem)> callback, bool doSelect)
-{
-    RootItem* rootItem = RootItem::instance();
-    ItemList<SimulatorItem> simulatorItems = rootItem->selectedItems<SimulatorItem>();
-
-    if(simulatorItems.empty()) {
-        simulatorItems.extractChildItems(RootItem::instance());
-        if(simulatorItems.empty()) {
-            mv->notify(_("There is no simulator item."));
-        } else if(simulatorItems.size() > 1) {
-            simulatorItems.clear();
-            mv->notify(_("Please select a simulator item to simulate."));
-        } else {
-            if(doSelect) {
-                rootItem->selectItem(simulatorItems.front());
-            }
-        }
-    }
-
-    typedef map<WorldItem*, SimulatorItem*> WorldToSimulatorMap;
-    WorldToSimulatorMap worldToSimulator;
-
-    for(size_t i = 0; i < simulatorItems.size(); i++) {
-        SimulatorItem* simulatorItem = simulatorItems.get(i);
-        WorldItem* world = simulatorItem->findOwnerItem<WorldItem>();
-        if(world) {
-            WorldToSimulatorMap::iterator p = worldToSimulator.find(world);
-            if(p == worldToSimulator.end()) {
-                worldToSimulator[world] = simulatorItem;
-            } else {
-                p->second = 0;
-            }
-        }
-    }
-
-    for(size_t i = 0; i < simulatorItems.size(); i++) {
-        SimulatorItem* simulatorItem = simulatorItems.get(i);
-        WorldItem* world = simulatorItem->findOwnerItem<WorldItem>();
-        if(!world) {
-            mv->notify(format(_("{} cannot be processed because it is not related with a world."), simulatorItem->name()));
-        } else {
-            WorldToSimulatorMap::iterator p = worldToSimulator.find(world);
-            if(p != worldToSimulator.end()) {
-                if(!p->second) {
-                    mv->notify(format(_("{} cannot be processed because another simulator"
-                                        "in the same world is also selected."),
-                                      simulatorItem->name()));
-                } else {
-                    callback(simulatorItem);
-                }
-            }
-        }
-    }
-}
-
-
-void SimulationManagerImpl::openDialogToLoadProject()
-{
-    ProjectManager* pm = ProjectManager::instance();
-    auto mw = MainWindow::instance();
-    int numItems = RootItem::instance()->countDescendantItems();
-    if(numItems > 0) {
-        QString title = _("Warning");
-        QString message;
-        QMessageBox::StandardButton clicked;
-        if(currentProjectFile.empty()) {
-            if(numItems == 1) {
-                message = _("A project item exists. "
-                            "Do you want to save it as a project file before loading a new project?");
-            } else {
-                message = _("Project items exist. "
-                            "Do you want to save them as a project file before loading a new project?");
-            }
-            clicked = QMessageBox::warning(
-                mw, title, message, QMessageBox::Save | QMessageBox::Cancel | QMessageBox::Ignore);
-        } else {
-            message = _("Project \"%1\" exists. Do you want to save it before loading a new project?");
-            clicked = QMessageBox::warning(
-                mw, title, message.arg(currentProjectName.c_str()),
-                QMessageBox::Save | QMessageBox::Cancel | QMessageBox::Ignore);
-        }
-        if(clicked == QMessageBox::Cancel) {
-            return;
-        }
-        if(clicked == QMessageBox::Save) {
-            pm->overwriteCurrentProject();
-        }
-    }
-
-    FileDialog dialog(mw);
-    dialog.setWindowTitle(_("Open a Choreonoid project file"));
-    dialog.setFileMode(QFileDialog::ExistingFile);
-    dialog.setViewMode(QFileDialog::List);
-    dialog.setLabelText(QFileDialog::Accept, _("Open"));
-    dialog.setLabelText(QFileDialog::Reject, _("Cancel"));
-
-    QStringList filters;
-    filters << _("Project files (*.cnoid)");
-    filters << _("Any files (*)");
-    dialog.setNameFilters(filters);
-
-    dialog.updatePresetDirectories();
-
-    if(dialog.exec()) {
-        pm->clearProject();
-        mv->flush();
-        string filename = dialog.selectedFiles().front().toStdString();
-        pm->loadProject(filename, nullptr);
-    }
-}
-
-
 void SimulationManagerImpl::onButton(const int& id, const bool& isPressed)
 {
     if(isPressed) {
         if(useStartButton->isChecked()) {
             if(id == Joystick::START_BUTTON) {
                 if(!startState) {
-                    startSimulation(true);
+                    sb->startSimulation(true);
+                    startState = true;
                 } else {
-                    pauseSimulation();
+                    if(simulatoritem) {
+                        if(!pauseState) {
+                            simulatoritem->pauseSimulation();
+                        } else {
+                            simulatoritem->restartSimulation();
+                        }
+                        pauseState = !pauseState;
+                    }
                 }
             } else if(id == Joystick::SELECT_BUTTON) {
                 if(!startState) {
-                    startSimulation(false);
+                    sb->startSimulation(false);
+                    startState = true;
                 } else {
-                    stopSimulation();
+                    if(simulatoritem) {
+                        simulatoritem->stopSimulation(true);
+                        startState = false;
+                        pauseState = false;
+                    }
                 }
             }
         }
         if(useLogoButton->isChecked()) {
             if(id == Joystick::LOGO_BUTTON) {
-                openDialogToLoadProject();
+                ProjectManager::instance()->showDialogToLoadProject();
                 startState = false;
                 pauseState = false;
             }
