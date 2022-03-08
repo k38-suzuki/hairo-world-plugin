c4470dfa4947b6888020f7f52ec8af9334c672f2
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2022-03-07 18:18:06 -0800

    remove bookmark-manager

--- src/BookmarkPlugin/BookmarkPlugin.cpp
+++ src/BookmarkPlugin/BookmarkPlugin.cpp
@@ -6,7 +6,6 @@
 #include <cnoid/Plugin>
 #include <fmt/format.h>
 #include "BookmarkList.h"
-#include "BookmarkManager.h"
 #include "HistoryManager.h"
 
 using namespace cnoid;
@@ -25,7 +24,6 @@ public:
     virtual bool initialize() override
     {
         BookmarkList::initialize(this);
-        BookmarkManager::initialize(this);
         HistoryManager::initialize(this);
         return true;
     }
