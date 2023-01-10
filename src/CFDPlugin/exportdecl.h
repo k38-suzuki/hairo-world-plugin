ac06a0dc218173cca766086a3845a508271b340b
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2023-01-10 06:56:26 -0800

    Fix exportdecl

--- src/CFDPlugin/exportdecl.h
+++ src/CFDPlugin/exportdecl.h
@@ -1,31 +1,31 @@
-#ifndef CNOID_CFD_PLUGIN_EXPORTDECL_H
-# define CNOID_CFD_PLUGIN_EXPORTDECL_H
+#ifndef CNOID_CFDPLUGIN_EXPORTDECL_H_INCLUDED
+# define CNOID_CFDPLUGIN_EXPORTDECL_H_INCLUDED
 # if defined _WIN32 || defined __CYGWIN__
-#  define CNOID_CFD_PLUGIN_DLLIMPORT __declspec(dllimport)
-#  define CNOID_CFD_PLUGIN_DLLEXPORT __declspec(dllexport)
-#  define CNOID_CFD_PLUGIN_DLLLOCAL
+#  define CNOID_CFDPLUGIN_DLLIMPORT __declspec(dllimport)
+#  define CNOID_CFDPLUGIN_DLLEXPORT __declspec(dllexport)
+#  define CNOID_CFDPLUGIN_DLLLOCAL
 # else
 #  if __GNUC__ >= 4
-#   define CNOID_CFD_PLUGIN_DLLIMPORT __attribute__ ((visibility("default")))
-#   define CNOID_CFD_PLUGIN_DLLEXPORT __attribute__ ((visibility("default")))
-#   define CNOID_CFD_PLUGIN_DLLLOCAL  __attribute__ ((visibility("hidden")))
+#   define CNOID_CFDPLUGIN_DLLIMPORT __attribute__ ((visibility("default")))
+#   define CNOID_CFDPLUGIN_DLLEXPORT __attribute__ ((visibility("default")))
+#   define CNOID_CFDPLUGIN_DLLLOCAL  __attribute__ ((visibility("hidden")))
 #  else
-#   define CNOID_CFD_PLUGIN_DLLIMPORT
-#   define CNOID_CFD_PLUGIN_DLLEXPORT
-#   define CNOID_CFD_PLUGIN_DLLLOCAL
+#   define CNOID_CFDPLUGIN_DLLIMPORT
+#   define CNOID_CFDPLUGIN_DLLEXPORT
+#   define CNOID_CFDPLUGIN_DLLLOCAL
 #  endif
 # endif
 
-# ifdef CNOID_CFD_PLUGIN_STATIC
-#  define CNOID_CFD_PLUGIN_DLLAPI
-#  define CNOID_CFD_PLUGIN_LOCAL
+# ifdef CNOID_CFDPLUGIN_STATIC
+#  define CNOID_CFDPLUGIN_DLLAPI
+#  define CNOID_CFDPLUGIN_LOCAL
 # else
 #  ifdef CnoidCFDPlugin_EXPORTS
-#   define CNOID_CFD_PLUGIN_DLLAPI CNOID_CFD_PLUGIN_DLLEXPORT
+#   define CNOID_CFDPLUGIN_DLLAPI CNOID_CFDPLUGIN_DLLEXPORT
 #  else
-#   define CNOID_CFD_PLUGIN_DLLAPI CNOID_CFD_PLUGIN_DLLIMPORT
+#   define CNOID_CFDPLUGIN_DLLAPI CNOID_CFDPLUGIN_DLLIMPORT
 #  endif
-#  define CNOID_CFD_PLUGIN_LOCAL CNOID_CFD_PLUGIN_DLLLOCAL
+#  define CNOID_CFDPLUGIN_LOCAL CNOID_CFDPLUGIN_DLLLOCAL
 # endif
 
 #endif
@@ -33,4 +33,4 @@
 #ifdef CNOID_EXPORT
 # undef CNOID_EXPORT
 #endif
-#define CNOID_EXPORT CNOID_CFD_PLUGIN_DLLAPI
+#define CNOID_EXPORT CNOID_CFDPLUGIN_DLLAPI
--- src/OMPLPlugin/exportdecl.h
+++ src/OMPLPlugin/exportdecl.h
@@ -1,31 +1,31 @@
-#ifndef CNOID_OMPL_PLUGIN_EXPORTDECL_H
-# define CNOID_OMPL_PLUGIN_EXPORTDECL_H
+#ifndef CNOID_OMPLPLUGIN_EXPORTDECL_H
+# define CNOID_OMPLPLUGIN_EXPORTDECL_H
 # if defined _WIN32 || defined __CYGWIN__
-#  define CNOID_OMPL_PLUGIN_DLLIMPORT __declspec(dllimport)
-#  define CNOID_OMPL_PLUGIN_DLLEXPORT __declspec(dllexport)
-#  define CNOID_OMPL_PLUGIN_DLLLOCAL
+#  define CNOID_OMPLPLUGIN_DLLIMPORT __declspec(dllimport)
+#  define CNOID_OMPLPLUGIN_DLLEXPORT __declspec(dllexport)
+#  define CNOID_OMPLPLUGIN_DLLLOCAL
 # else
 #  if __GNUC__ >= 4
-#   define CNOID_OMPL_PLUGIN_DLLIMPORT __attribute__ ((visibility("default")))
-#   define CNOID_OMPL_PLUGIN_DLLEXPORT __attribute__ ((visibility("default")))
-#   define CNOID_OMPL_PLUGIN_DLLLOCAL  __attribute__ ((visibility("hidden")))
+#   define CNOID_OMPLPLUGIN_DLLIMPORT __attribute__ ((visibility("default")))
+#   define CNOID_OMPLPLUGIN_DLLEXPORT __attribute__ ((visibility("default")))
+#   define CNOID_OMPLPLUGIN_DLLLOCAL  __attribute__ ((visibility("hidden")))
 #  else
-#   define CNOID_OMPL_PLUGIN_DLLIMPORT
-#   define CNOID_OMPL_PLUGIN_DLLEXPORT
-#   define CNOID_OMPL_PLUGIN_DLLLOCAL
+#   define CNOID_OMPLPLUGIN_DLLIMPORT
+#   define CNOID_OMPLPLUGIN_DLLEXPORT
+#   define CNOID_OMPLPLUGIN_DLLLOCAL
 #  endif
 # endif
 
-# ifdef CNOID_OMPL_PLUGIN_STATIC
-#  define CNOID_OMPL_PLUGIN_DLLAPI
-#  define CNOID_OMPL_PLUGIN_LOCAL
+# ifdef CNOID_OMPLPLUGIN_STATIC
+#  define CNOID_OMPLPLUGIN_DLLAPI
+#  define CNOID_OMPLPLUGIN_LOCAL
 # else
 #  ifdef CnoidOMPLPlugin_EXPORTS
-#   define CNOID_OMPL_PLUGIN_DLLAPI CNOID_OMPL_PLUGIN_DLLEXPORT
+#   define CNOID_OMPLPLUGIN_DLLAPI CNOID_OMPLPLUGIN_DLLEXPORT
 #  else
-#   define CNOID_OMPL_PLUGIN_DLLAPI CNOID_OMPL_PLUGIN_DLLIMPORT
+#   define CNOID_OMPLPLUGIN_DLLAPI CNOID_OMPLPLUGIN_DLLIMPORT
 #  endif
-#  define CNOID_OMPL_PLUGIN_LOCAL CNOID_OMPL_PLUGIN_DLLLOCAL
+#  define CNOID_OMPLPLUGIN_LOCAL CNOID_OMPLPLUGIN_DLLLOCAL
 # endif
 
 #endif
@@ -33,4 +33,4 @@
 #ifdef CNOID_EXPORT
 # undef CNOID_EXPORT
 #endif
-#define CNOID_EXPORT CNOID_OMPL_PLUGIN_DLLAPI
+#define CNOID_EXPORT CNOID_OMPLPLUGIN_DLLAPI
