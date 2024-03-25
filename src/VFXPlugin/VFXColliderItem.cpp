89e56aa1955c071453e1d423fe6a2b5b5b94ecfd
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2024-03-25 21:45:30 +0900

    Rename var

--- src/VFXPlugin/VFXColliderItem.cpp
+++ src/VFXPlugin/VFXColliderItem.cpp
@@ -33,76 +33,76 @@ void onSelectedItemChanged(const ItemList<>& selectedItems)
     GeneralSliderView* sliderView = GeneralSliderView::instance();
     sliders.clear();
 
-    ItemList<VFXColliderItem> areaItems = selectedItems;
-    for(auto& areaItem : areaItems) {
-        const Vector3& hsv = areaItem->hsv();
-        const Vector3& rgb = areaItem->rgb();
+    ItemList<VFXColliderItem> colliders = selectedItems;
+    for(auto& collider : colliders) {
+        const Vector3& hsv = collider->hsv();
+        const Vector3& rgb = collider->rgb();
 
         GeneralSliderView::SliderPtr slider_hue = sliderView->getOrCreateSlider(_("Hue"), effect_range[0][0], effect_range[0][1], 2);
         slider_hue->setValue(hsv[0]);
         slider_hue->setCallback([=](double value){
-            areaItem->setHsv(Vector3(value, hsv[1], hsv[2]));
-            areaItem->notifyUpdate(); });
+            collider->setHsv(Vector3(value, hsv[1], hsv[2]));
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_saturation = sliderView->getOrCreateSlider(_("Saturation"), effect_range[1][0], effect_range[1][1], 2);
         slider_saturation->setValue(hsv[1]);
         slider_saturation->setCallback([=](double value){
-            areaItem->setHsv(Vector3(hsv[0], value, hsv[2]));
-            areaItem->notifyUpdate(); });
+            collider->setHsv(Vector3(hsv[0], value, hsv[2]));
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_value = sliderView->getOrCreateSlider(_("Value"), effect_range[2][0], effect_range[2][1], 2);
         slider_value->setValue(hsv[2]);
         slider_value->setCallback([=](double value){
-            areaItem->setHsv(Vector3(hsv[0], hsv[1], value));
-            areaItem->notifyUpdate(); });
+            collider->setHsv(Vector3(hsv[0], hsv[1], value));
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_red = sliderView->getOrCreateSlider(_("Red"), effect_range[3][0], effect_range[3][1], 2);
         slider_red->setValue(rgb[0]);
         slider_red->setCallback([=](double value){
-            areaItem->setRgb(Vector3(value, rgb[1], rgb[2]));
-            areaItem->notifyUpdate(); });
+            collider->setRgb(Vector3(value, rgb[1], rgb[2]));
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_green = sliderView->getOrCreateSlider(_("Green"), effect_range[4][0], effect_range[4][1], 2);
         slider_green->setValue(rgb[1]);
         slider_green->setCallback([=](double value){
-            areaItem->setRgb(Vector3(rgb[0], value, rgb[2]));
-            areaItem->notifyUpdate(); });
+            collider->setRgb(Vector3(rgb[0], value, rgb[2]));
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_blue = sliderView->getOrCreateSlider(_("Blue"), effect_range[5][0], effect_range[5][1], 2);
         slider_blue->setValue(rgb[2]);
         slider_blue->setCallback([=](double value){
-            areaItem->setRgb(Vector3(rgb[0], rgb[1], value));
-            areaItem->notifyUpdate(); });
+            collider->setRgb(Vector3(rgb[0], rgb[1], value));
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_coefb = sliderView->getOrCreateSlider(_("CoefB"), effect_range[6][0], effect_range[6][1], 2);
-        slider_coefb->setValue(areaItem->coefB());
+        slider_coefb->setValue(collider->coefB());
         slider_coefb->setCallback([=](double value){
-            areaItem->setCoefB(value);
-            areaItem->notifyUpdate(); });
+            collider->setCoefB(value);
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_coefd = sliderView->getOrCreateSlider(_("CoefD"), effect_range[7][0], effect_range[7][1], 2);
-        slider_coefd->setValue(areaItem->coefD());
+        slider_coefd->setValue(collider->coefD());
         slider_coefd->setCallback([=](double value){
-            areaItem->setCoefD(value);
-            areaItem->notifyUpdate(); });
+            collider->setCoefD(value);
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_stddev = sliderView->getOrCreateSlider(_("Std_dev"), effect_range[8][0], effect_range[8][1], 2);
-        slider_stddev->setValue(areaItem->stdDev());
+        slider_stddev->setValue(collider->stdDev());
         slider_stddev->setCallback([=](double value){
-            areaItem->setStdDev(value);
-            areaItem->notifyUpdate(); });
+            collider->setStdDev(value);
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_salt = sliderView->getOrCreateSlider(_("Salt"), effect_range[9][0], effect_range[9][1], 2);
-        slider_salt->setValue(areaItem->salt());
+        slider_salt->setValue(collider->salt());
         slider_salt->setCallback([=](double value){
-            areaItem->setSalt(value);
-            areaItem->notifyUpdate(); });
+            collider->setSalt(value);
+            collider->notifyUpdate(); });
 
         GeneralSliderView::SliderPtr slider_pepper = sliderView->getOrCreateSlider(_("Pepper"), effect_range[10][0], effect_range[10][1], 2);
-        slider_pepper->setValue(areaItem->pepper());
+        slider_pepper->setValue(collider->pepper());
         slider_pepper->setCallback([=](double value){
-            areaItem->setPepper(value);
-            areaItem->notifyUpdate(); });
+            collider->setPepper(value);
+            collider->notifyUpdate(); });
 
         sliders.push_back(slider_hue);
         sliders.push_back(slider_saturation);
